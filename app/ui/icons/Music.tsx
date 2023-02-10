e156793ca651a306d1cba79f69461953b2820ed9
Johann Rakotoharisoa <johann.rakotoharisoa@gmail.com>
2023-02-10 14:40:25 +0100

    🍱 fix icons attributes

--- app/ui/icons/Music.tsx
+++ app/ui/icons/Music.tsx
@@ -5,9 +5,9 @@ export const MusicIcon: React.FC<React.SVGAttributes<SVGElement>> = (props) => {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       {...props}
     >
       <path d="M9 18V5l12-2v13"></path>
--- app/ui/icons/Playlist.tsx
+++ app/ui/icons/Playlist.tsx
@@ -6,9 +6,9 @@ export const PlaylistIcon: React.FC<React.SVGAttributes<SVGElement>> = (
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
     {...props}
   >
     <path d="M21 15V6"></path>
