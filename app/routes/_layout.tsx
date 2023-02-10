e5da1370ca25bdfeb1c5355d8d310585a6ed93d0
Johann Rakotoharisoa <johann.rakotoharisoa@gmail.com>
2023-02-10 14:44:07 +0100

    ♻️ move playlist to loader

--- app/routes/_layout.tsx
+++ app/routes/_layout.tsx
@@ -1,8 +1,28 @@
-import { Outlet } from "@remix-run/react";
+import { LoaderArgs } from "@remix-run/node";
+import { Outlet, useLoaderData } from "@remix-run/react";
 import { MusicIcon } from "~/ui/icons/Music";
 import { PlaylistIcon } from "~/ui/icons/Playlist";
 
+export const loader = ({}: LoaderArgs) => {
+  return [
+    "Recently Added",
+    "Recently Played",
+    "Top Songs",
+    "Top Albums",
+    "Top Artists",
+    "Logic Discography",
+    "Bedtime Beats",
+    "Feeling Happy",
+    "button>",
+    "Runtober",
+    "Mellow Days",
+    "Eminem Essentials",
+  ];
+};
+
 export default function Layout() {
+  const playlists = useLoaderData<typeof loader>();
+
   return (
     <div className="grid grid-cols-4 xl:grid-cols-5 h-full">
       <aside>
@@ -23,13 +43,13 @@ export default function Layout() {
             >
               <div>
                 <div className="space-y-1 p-2">
-                  {playlists.map((p) => (
+                  {playlists.map((playlist) => (
                     <button
-                      key={p}
+                      key={playlist}
                       className="inline-flex items-center text-sm transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none bg-transparent hover:bg-slate-100  data-[state=open]:bg-transparent  h-9 px-2 rounded-md w-full justify-start font-normal"
                     >
                       <PlaylistIcon className="mr-2 h-4 w-4" />
-                      {p}
+                      {playlist}
                     </button>
                   ))}
                 </div>
@@ -39,24 +59,9 @@ export default function Layout() {
         </div>
       </aside>
 
-      <div className="h-full col-span-3 border-l border-l-slate-200 xl:col-span-4">
+      <main className="h-full col-span-3 border-l border-l-slate-200 xl:col-span-4 p-6">
         <Outlet />
-      </div>
+      </main>
     </div>
   );
 }
-
-const playlists = [
-  "Recently Added",
-  "Recently Played",
-  "Top Songs",
-  "Top Albums",
-  "Top Artists",
-  "Logic Discography",
-  "Bedtime Beats",
-  "Feeling Happy",
-  "button>",
-  "Runtober",
-  "Mellow Days",
-  "Eminem Essentials",
-];
