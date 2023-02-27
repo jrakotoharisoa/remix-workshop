89c23f1ecdcef5cc93ff2172453e9df9a4e8464b
Johan Rouve <johan.rouve@comet.team>
2023-02-27 10:02:09 +0100

    🐛 Fix tailwind import

--- app/root.tsx
+++ app/root.tsx
@@ -1,12 +1,5 @@
 import type { LinksFunction, MetaFunction } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
@@ -14,11 +7,9 @@ export const meta: MetaFunction = () => ({
   viewport: "width=device-width,initial-scale=1",
 });
 
-import stylesheet from "~/tailwind.css";
+import stylesheet from "~/styles/tailwind.css";
 
-export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: stylesheet },
-];
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: stylesheet }];
 
 export default function App() {
   return (
